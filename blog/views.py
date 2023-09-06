9c9625d374f3751bf1f46ef4e3c0d5b51e9eed6f
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2023-09-06 22:21:18 +0530

    added models

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,26 @@
-from django.shortcuts import render
+from datetime import date
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+
 
 # Create your views here.
 
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    latest_posts = Post.objects.all().order_by("-date")[:3]
+
+    return render(request, "blog/index.html", {"posts": latest_posts})
 
 
 def posts(request):
-    pass
+    all_posts = Post.objects.all().order_by("-date")
+    return render(request, "blog/all-posts.html", {"all_posts": all_posts})
 
 
 def post_detail(request, slug):
-    pass
+    identified_post = get_object_or_404(Post, slug=slug)
+    return render(
+        request,
+        "blog/post-detail.html",
+        {"post": identified_post, "post_tags": identified_post.tags.all()},
+    )
