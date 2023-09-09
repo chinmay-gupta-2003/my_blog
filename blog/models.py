53c1496a3559cde95c00d03f1806808d8b2d34b0
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2023-09-09 15:27:19 +0530

    comment functionality setup

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
 
-# Create your models here.
-
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
@@ -13,7 +11,7 @@ class Author(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=100)
     excerpt = models.CharField(max_length=200)
-    img = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
@@ -25,3 +23,10 @@ class Post(models.Model):
 
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
+
+
+class Comments(models.Model):
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField()
+    text = models.TextField(max_length=200)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
