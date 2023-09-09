4d48dd421fe09e6c828207ecdd461a050f6b8974
chinmay-gupta-2003 <guptachinmay2003@gmail.com>
2023-09-09 15:26:38 +0530

    admin configuration

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Tag, Author
+from .models import Post, Tag, Author, Comments
 
 
 # Register your models here.
@@ -10,3 +10,4 @@ class PostAdmin(admin.ModelAdmin):
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
 admin.site.register(Author)
+admin.site.register(Comments)
